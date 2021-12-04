e20532a6a7107ebba875d637cd450b3d0df06cf3
firstname lastname <address@email.com>
2021-12-04 13:46:36 -0500

    Capstone Brewery class updated

---
+++ final-capstone/dotnet/Capstone/Models/Brewery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Brewery
+    {
+        public int BreweryID { get; set; }
+        public int BreweryOwnerID { get; set; }
+        public string BrewerName { get; set; }
+        public string BreweryName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Address { get; set; }
+        public string HoursOfOperation { get; set; }
+        public string DaysOfOperation { get; set; }
+
+    }
+}
