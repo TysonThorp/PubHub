1c0072fc555a05762089916a7ce89a85e94f639d
Levi Swartz <levi@leviswartz.net>
2021-12-04 13:24:41 -0500

    Add properties to User model

--- final-capstone/dotnet/Capstone/Models/User.cs
+++ final-capstone/dotnet/Capstone/Models/User.cs
@@ -1,11 +1,16 @@
-﻿namespace Capstone.Models
+﻿using System;
+
+namespace Capstone.Models
 {
     public class User
     {
         public int UserId { get; set; }
         public string Username { get; set; }
+        public string EmailAddress { get; set; }
         public string PasswordHash { get; set; }
+        public string PhoneNumber { get; set; }
         public string Salt { get; set; }
+        public DateTime Birthday { get; set; }
         public string Role { get; set; }
     }
 
