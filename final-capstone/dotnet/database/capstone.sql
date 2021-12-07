1d24a5b2758514e6cc0de69c1a6b15472e38055f
Ryan DuPerow <rkdupe@gmail.com>
2021-12-07 16:26:56 -0500

    Updated decimal column in beer table

--- final-capstone/dotnet/database/capstone.sql
+++ final-capstone/dotnet/database/capstone.sql
@@ -51,7 +51,7 @@ CREATE TABLE beer(
 	beer_name varchar(30) NOT NULL,
 	description varchar(150) NOT NULL,
 	image varchar(150) NOT NULL,
-	abv varchar(5) NOT NULL,
+	abv decimal(5,1) NOT NULL,
 	beer_type varchar(25) NOT NULL,
 	PRIMARY KEY (beer_id)
 	)
