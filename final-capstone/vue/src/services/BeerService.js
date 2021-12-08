73ff54f0e4216ed3877acc0d36f903c15d9ab1f1
Ryan DuPerow <rkdupe@gmail.com>
2021-12-08 12:29:24 -0500

    Create BeerService

---
+++ final-capstone/vue/src/services/BeerService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default {
+
+  getAllBeers() {
+    return axios.get('/beers')
+  },
+
+  getBeerById(beerId) {
+    return axios.get('/beers/${beerId}')
+  },
+
+  addBeer(beer) {
+      return axios.post('/beers', beer)
+  },
+
+  deleteBeer(beerId) {
+      return axios.delete('/beers/${beerId}')
+  }
+
+}
