a0c5835bcd333b22d0a47f44322aa0c30c88a46d
Ryan DuPerow <rkdupe@gmail.com>
2021-12-09 17:29:30 -0500

    Created Beer Detail Component

---
+++ final-capstone/vue/src/components/BeerDetail.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>
+            {{ beer.beerName }}
+        </h1>
+        <ul>
+            <li>{{ beer.description }}</li>
+            <li>{{ beer.beerType }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import BeerService from '../services/BeerService.js'
+
+export default {
+    name: 'beer-detail',
+    data() {
+        return {
+            beer: {
+                beerName: '',
+                beerType: '',
+                description: '',
+                image: ''
+            }
+        }
+    },
+    created() {
+        const beerId = this.$route.params.breweryId;
+
+        BeerService.getBeerById(beerId).then((response) => {
+            this.beer = response.data;
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
