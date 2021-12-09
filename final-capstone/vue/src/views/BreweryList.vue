284d2cfa500a07a10e9c4d8e08e45a83c667e8f8
Levi Swartz <levi@leviswartz.net>
2021-12-09 08:44:30 -0500

    Create example /brewerylist page on frontend

---
+++ final-capstone/vue/src/views/BreweryList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul id="brewery-list">
+        <li v-for="brewery in breweries" :key="brewery.breweryId">
+            <div>
+                <h2 id="breweryName">{{ brewery.breweryName }}</h2>
+                <p id="breweryInfo">
+                    <a :href= brewery.website target="_blank">Website</a> |
+                    {{ brewery.phoneNumber }} |
+                    {{ brewery.address }}
+                </p>
+                <p id="breweryDesc">{{ brewery.description }}</p>
+            </div>
+            
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    import BreweryService from '../services/BreweryService';
+
+    export default {
+        name: 'Breweries',
+        components: {},
+        data() {
+            return {
+                breweries: []
+            }
+        },
+        created(){
+            BreweryService.getAllBreweries().then(response => {
+                    this.breweries = response.data;
+            });
+        }
+    }   
+</script>
