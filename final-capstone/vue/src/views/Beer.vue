6e85e589758fb2cc3f65220748cea2d95b8d167d
Ryan DuPerow <rkdupe@gmail.com>
2021-12-09 17:30:26 -0500

    Created Beer View

---
+++ final-capstone/vue/src/views/Beer.vue
@@ -0,0 +1,18 @@
+<template>
+    <beer-detail />
+</template>
+
+<script>
+import BeerDetail from '../components/BeerDetail.vue'
+
+export default {
+  components: { 
+      BeerDetail 
+  },
+
+}
+</script>
+
+<style>
+
+</style>
