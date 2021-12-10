2e6283c2b96f9f541cb1901a7039cd0856865444
firstname lastname <address@email.com>
2021-12-10 14:59:16 -0500

    Review List Item and Review List Vue done

---
+++ final-capstone/vue/src/views/Reviews.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="reviews">
+     <h1>Reviewss</h1>
+     <review-list />
+  </div>
+</template>
+
+<script>
+
+import ReviewList from "../components/ReviewList.vue"
+
+export default {
+    components:{
+        ReviewList
+    }
+
+}
+</script>
+
+<style>
+
+</style>
