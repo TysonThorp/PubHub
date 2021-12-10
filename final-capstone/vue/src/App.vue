c67fb4b789044b4c0b51a1351ed48c844124ef91
Mustafa Abuhilal <mhilalosu@gmail.com>
2021-12-10 08:55:48 -0500

    app.vue work

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -1,57 +1,97 @@
 <template>
-  <v-app>
-    <h1>testing the header from app.vue right hurr</h1>
-    <v-app-bar
-      app
-      color="#000"
-      dark
-    >
-      <div class="d-flex align-center">
-        <div class="logo">
-          <span class="pub">Pub</span>
-          <span class="hub">Hub</span>
-        </div>
+  <v-app id="inspire">
+    
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+      <div class="logo">
+          Pub<span class="hub">Hub</span>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
+     <v-navigation-drawer v-model="drawer">
+       
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          to="/login"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+     </v-navigation-drawer>
 
     <v-main>
-      <router-view/>
+      <router-view></router-view>
+      <!--  -->
     </v-main>
   </v-app>
 </template>
 
+
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    data: () => ({ 
+      drawer: false,
+      items: [
+        {title: 'Sign-in' , icon: 'mdi-view-dashboard'},
+        {title: 'Beer' , icon: 'mdi-image'},
+        {title: 'Brewery' , icon: 'mdi-image'},
+      ],
+    }),
+    methods:{
+      toggleDrawer(){
+        this.drawer = !this.drawer;
+        console.log(this.drawer);
+      },
+    }  
+  }
 </script>
 
-<style lang="css" scoped>
+<style lang="css">
+
 .logo{
-  /* background: #f09; */
-  padding: 10px;
-  font-size: 24px;
   font-weight: bold;
+  font-size: 20px;
+  color: #fff;
+  background-color: #000;
+  border-radius: 5px;
+  padding: 10px;
 }
 
-.pub{
-  color:darkorange;
+.hub{
+  border-radius:1.75px;
+  background-color: orange;
+  color: #000;
 }
+
+.v-navigation-drawer{
+  position: absolute;
+}
+
+
+
 
 </style>
--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -1,37 +1,26 @@
 <template>
-  <div>
-    <v-container>
-      <v-row>
-        <v-col>
-          <h1>One Column</h1>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <h2>This is an awesome feature!</h2>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore molestiae, quidem quas asperiores illum amet. Ipsum laborum voluptates mollitia quod asperiores. Neque cum libero corporis nobis saepe quisquam tempore veritatis.
-        </v-col>
-        <v-col>
-          column two
-        </v-col>
-        <v-col>
-          column three
-        </v-col>
-      </v-row>
-    </v-container>
-    <HelloWorld/>
-  <h1>Hello there you there?</h1>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
-    name: 'home',
-
-    components: {
-      HelloWorld,
-    },
+    data: () => ({ drawer: null }),
   }
 </script>
