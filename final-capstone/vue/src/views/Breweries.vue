4a3f15d4d0607395dd71f1ea238174c54091cc7f
Ryan DuPerow <rkdupe@gmail.com>
2021-12-10 15:41:57 -0500

    Deleted info from BreweryList View

---
+++ final-capstone/vue/src/views/Breweries.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <brewery-list />
+  </v-container>  
+</template>
+
+<script>
+import BreweryList from "../components/BreweryList.vue"
+
+export default {
+  name: "Breweries",
+  components: { 
+      BreweryList 
+  }
+};
+</script>
+
+<style>
+</style>
