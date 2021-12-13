7d29b8da345ab17cc73651d2849194607e5bbdb0
Levi Swartz <levi@leviswartz.net>
2021-12-13 15:42:06 -0500

    Display BeerDetail component in a different format in individual Beer view

--- final-capstone/vue/src/components/BeerDetail.vue
+++ final-capstone/vue/src/components/BeerDetail.vue
@@ -1,16 +1,31 @@
 <template>
-    <v-card class="beerItem" :to="link">
-        <div>
-        <v-card-title><h3>{{beer.beerName}}</h3></v-card-title>
-        <v-card-subtitle>{{ beer.beerType }}</v-card-subtitle>
-        <v-card-text>
-            {{ beer.description }} ABV: {{beer.abv }} 
-        </v-card-text>
+    <div>
+        <div v-if="context === 'beer-individual'" class="beerItem">
+            <div>
+                <h1 style="font-size: 1.8em">{{beer.beerName}}</h1>
+                <p>{{ beer.beerType }} | ABV: {{beer.abv }}</p>
+                <p>{{ beer.description }}</p>
+            </div>
+            <div style="margin-left: auto; display: flex; align-items: center;">
+                <img :src='"/img/"+ beer.image' class="beer-image">
+            </div>
         </div>
-        <div style="margin-left: auto; display: flex; align-items: center;">
-        <img :src='"/img/"+ beer.image' class="beer-image">
-        </div>
-    </v-card>
+
+        <v-card v-else class="beerItem" :to="link">
+            <div>
+                <v-card-title><h3>{{beer.beerName}}</h3></v-card-title>
+                <v-card-subtitle>{{ beer.beerType }}</v-card-subtitle>
+                <v-card-text>
+                {{ beer.description }} ABV: {{beer.abv }} 
+                </v-card-text>
+            </div>
+            <div style="margin-left: auto; display: flex; align-items: center;">
+                <img :src='"/img/"+ beer.image' class="beer-image">
+            </div>
+        </v-card>
+
+        
+    </div>
 </template>
 
 <script>
@@ -19,7 +34,8 @@ import BeerService from '../services/BeerService.js'
 export default {
     name: 'beer-detail',
     props: {
-        beerId: Number
+        beerId: Number,
+        context: String
     },
     data() {
         return {
