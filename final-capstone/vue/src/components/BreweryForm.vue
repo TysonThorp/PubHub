01e584a5a433a6b162f136b46c37984222dc023c
Ryan DuPerow <rkdupe@gmail.com>
2021-12-14 17:42:59 -0500

    Created BreweryForm Component, still have lots of work to do

---
+++ final-capstone/vue/src/components/BreweryForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import BreweryService from '../services/BreweryService'
+
+export default {
+    name: 'brewery-form',
+    data() {
+        return {
+            brewery: {
+                breweryName: '',
+                emailAddress: '',
+                phoneNumber: '',
+                website: '',
+                description: '',
+                hoursOfOperation: '',
+                address: ''
+            }
+        }
+    },
+    created() {
+        
+    },
+    methods: {
+        createBrewery() {
+            BreweryService.addBrewery(this.brewery).then((response) => {
+                
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
