2fb731f2f8453df62d8653cf884c280ab7fe7a69
Ryan DuPerow <rkdupe@gmail.com>
2021-12-15 17:35:03 -0500

    Removed UpdateBrewery from Brewery View

--- final-capstone/vue/src/views/Brewery.vue
+++ final-capstone/vue/src/views/Brewery.vue
@@ -1,7 +1,6 @@
 <template>
     <div v-if="isLoaded">
         <brewery-detail :breweryId="brewery.breweryId" context="brewery-individual"/>
-        <update-brewery :breweryId="brewery.breweryId"/>
         <h2>Beers:</h2>
         <beer-list :breweryId="brewery.breweryId" :showDelete="isBrewer"/>
     </div>
@@ -11,14 +10,14 @@
 import BreweryDetail from '../components/BreweryDetail.vue';
 import BreweryService from '../services/BreweryService.js';
 import BeerList from '../components/BeerList.vue';
-import UpdateBrewery from '../components/UpdateBrewery.vue'
+
 
 
 export default {
     components: { 
         BreweryDetail, 
-        BeerList,
-        UpdateBrewery
+        BeerList
+
     },
     data() {
         return {
