e2624a71a0696752e06d50a165ce070a4b850a8d
Ryan DuPerow <rkdupe@gmail.com>
2021-12-15 08:01:20 -0500

    Added BreweryForm to Breweries View

--- final-capstone/vue/src/views/Breweries.vue
+++ final-capstone/vue/src/views/Breweries.vue
@@ -6,16 +6,21 @@
       </div>
         <h1>Breweries</h1>
       <brewery-list :showAll="true"/>
+      <div v-if="this.$store.state.user.role === 'admin'">
+        <brewery-form />
+      </div>
     </div>
 </template>
 
 <script>
 import BreweryList from "../components/BreweryList.vue"
+import BreweryForm from "../components/BreweryForm.vue"
 
 export default {
   name: "Breweries",
   components: { 
-      BreweryList 
+      BreweryList,
+      BreweryForm 
   }
 };
 </script>
