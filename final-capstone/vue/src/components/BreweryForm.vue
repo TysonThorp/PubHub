37a63e9fa4df61ad5ccf2faa0b64e5b22d695208
Ryan DuPerow <rkdupe@gmail.com>
2021-12-15 09:15:26 -0500

    Still working on New Brewery Form....

--- final-capstone/vue/src/components/BreweryForm.vue
+++ final-capstone/vue/src/components/BreweryForm.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
+  
+  <v-form v-on:submit.prevent='createBrewery'>
+      <h3>
+          Add Brewery:
+      </h3>
+      <v-text-field label="Brewery Name" v-model="brewery.breweryName"></v-text-field>
+      <v-text-field label="Brewery Owner ID" v-model="brewery.breweryOwnerId"></v-text-field>
+      <v-text-field label="Email" v-model="brewery.emailAddress"></v-text-field>
+      <v-text-field label="Phone" v-model="brewery.phoneNumber"></v-text-field>
+      <v-text-field label="Website" v-model="brewery.website"></v-text-field>
+      <v-textarea label="Description" v-model="brewery.description"></v-textarea>
+      <v-text-field label="Hours of Operation" v-model="brewery.hoursOfOperation"></v-text-field>
+      <v-text-field label="Address" v-model="brewery.address"></v-text-field>
 
-  </div>
+      <v-btn type="submit">Submit</v-btn>
+  </v-form>
 </template>
 
 <script>
@@ -13,24 +26,43 @@ export default {
         return {
             brewery: {
                 breweryName: '',
+                breweryOwnerId: '',
                 emailAddress: '',
                 phoneNumber: '',
                 website: '',
                 description: '',
                 hoursOfOperation: '',
-                address: ''
+                address: '',
+                isActive: 1
             }
         }
     },
-    created() {
-        
-    },
     methods: {
         createBrewery() {
-            BreweryService.addBrewery(this.brewery).then((response) => {
-                
+            const newBrewery = {
+                breweryName: this.brewery.breweryName,
+                breweryOwnerId: this.brewery.breweryOwnerId,
+                emailAddress: this.brewery.emailAddress,
+                phoneNumber: this.brewery.phoneNumber,
+                website: this.brewery.website,
+                description: this.brewery.description,
+                hoursOfOperation: this.brewery.hoursOfOperation,
+                address: this.brewery.address,
+                isActive: this.brewery.isActive
+            }
+            BreweryService.addBrewery(newBrewery).then((response) => {
+                if (response.status === 201) {
+                    this.$router.go();
+                }               
             })
         }
+    },
+    created() {
+        // if (this.breweryId != 0) {
+        //     BreweryService.getBreweryById(this.breweryId).then((response) => {
+        //         this.brewery = response.data;
+        //     })
+        // }
     }
 }
 </script>
