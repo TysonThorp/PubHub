5f9b1d56799c94df4415a68bb52e4c22a4f700dc
Ryan DuPerow <rkdupe@gmail.com>
2021-12-15 13:17:59 -0500

    Got form to work and added functionality to hide the form

--- final-capstone/vue/src/components/BreweryForm.vue
+++ final-capstone/vue/src/components/BreweryForm.vue
@@ -1,69 +1,68 @@
 <template>
-  
-  <v-form v-on:submit.prevent>
-      <h3>
-          Add Brewery:
-      </h3>
-      <v-text-field label="Brewery Name" v-model="brewery.breweryName"></v-text-field>
-      <v-text-field label="Brewery Owner ID" v-model="brewery.breweryOwnerId"></v-text-field>
+  <div>
+    <v-btn v-on:click="showForm = !showForm">Add Brewery</v-btn>
+    <v-form v-if="showForm == true">
+      <v-text-field
+        label="Brewery Name"
+        v-model="brewery.breweryName"
+      ></v-text-field>
+      <v-text-field
+        label="Brewery Owner ID"
+        v-model.number="brewery.breweryOwnerId"
+      ></v-text-field>
       <v-text-field label="Email" v-model="brewery.emailAddress"></v-text-field>
       <v-text-field label="Phone" v-model="brewery.phoneNumber"></v-text-field>
       <v-text-field label="Website" v-model="brewery.website"></v-text-field>
-      <v-textarea label="Description" v-model="brewery.description"></v-textarea>
-      <v-text-field label="Hours of Operation" v-model="brewery.hoursOfOperation"></v-text-field>
+      <v-textarea
+        label="Description"
+        v-model="brewery.description"
+      ></v-textarea>
+      <v-text-field
+        label="Hours of Operation"
+        v-model="brewery.hoursOfOperation"
+      ></v-text-field>
       <v-text-field label="Address" v-model="brewery.address"></v-text-field>
 
       <v-btn type="submit" v-on:click="createBrewery">Submit</v-btn>
-  </v-form>
+    </v-form>
+  </div>
 </template>
 
 <script>
-import BreweryService from '../services/BreweryService'
+import BreweryService from "../services/BreweryService";
 
 export default {
-    name: 'brewery-form',
-    data() {
-        return {
-            brewery: {
-                breweryName: 'test',
-                breweryOwnerId: this.breweryOwnerId,
-                emailAddress: 'test',
-                phoneNumber: 'test',
-                website: 'test',
-                description: 'test',
-                hoursOfOperation: 'test',
-                address: 'test',
-                image: 'ListermannExterior.JPG',
-                imageTwo: 'ListermannInterior.JPG', 
-                imageThree: 'ListermannTaproom.JPG',
-                isActive: true
-            }
+  name: "brewery-form",
+  data() {
+    return {
+      brewery: {
+        breweryName: "",
+        breweryOwnerId: 0,
+        emailAddress: "",
+        phoneNumber: "",
+        website: "",
+        description: "",
+        hoursOfOperation: "",
+        address: "",
+        image: "ListermannExterior.JPG",
+        imageTwo: "ListermannInterior.JPG",
+        imageThree: "ListermannTaproom.JPG",
+        isActive: true,
+      },
+      showForm: false,
+    };
+  },
+  methods: {
+    createBrewery() {
+      BreweryService.addBrewery(this.brewery).then((response) => {
+        if (response.status == 200) {
+          this.$router.go();
         }
+      });
     },
-    methods: {
-        createBrewery() {
-            // const newBrewery = {
-            //     breweryName: this.brewery.breweryName,
-            //     breweryOwnerId: this.brewery.breweryOwnerId,
-            //     emailAddress: this.brewery.emailAddress,
-            //     phoneNumber: this.brewery.phoneNumber,
-            //     website: this.brewery.website,
-            //     description: this.brewery.description,
-            //     hoursOfOperation: this.brewery.hoursOfOperation,
-            //     address: this.brewery.address,
-            //     isActive: this.brewery.isActive
-            // }
-            BreweryService.addBrewery(this.brewery).then((response) => {
-                if (response.status == 200) {
-                    this.brewery = this.response.data;
-                }
-                               
-            })
-        }
-    },
-}
+  },
+};
 </script>
 
 <style>
-    
 </style>
